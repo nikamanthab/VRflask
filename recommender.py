ec6f487d3ce90bccbe65c7f952e7d51c390e47f0
nikamanthab <nitinnikamanth@gmail.com>
2019-09-19 13:29:16 +0000

    postman tested

--- recommender.py
+++ recommender.py
@@ -12,17 +12,22 @@ class recommender:
         self.user_similarity = None
     def createMoviesDF(self,values):
         self.movies = pd.DataFrame(np.array(values),columns=["Movies","movieid"])
+        print(self.movies)
         return self.movies
     def createUserRatingDF(self,values):
         self.userrating = pd.DataFrame(np.array(values),columns=["userid","movieid","rating"])
+        print(self.userrating)
         return self.userrating
     def mergeDF(self):
+        print("yoyo")
         self.merged_df = pd.merge(self.userrating,self.movies)
+        print(self.merged_df)
         return self.merged_df
     def createDataMatrix(self):
-        n_movies = len(self.merged_df['Movies'].unique())
-        n_users = len(self.merged_df['userid'].unique())
+        n_movies = len(self.movies['Movies'].unique())
+        n_users = len(self.userrating['userid'].unique())
         self.data_matrix = np.zeros((n_users, n_movies))
+        print("data_matrix:",self.data_matrix)
         
         for line in self.merged_df.itertuples():
             print(line[1]-1, line[2], line[3])
@@ -30,7 +35,9 @@ class recommender:
         return self.data_matrix
     def generateUserSimilarityMatrix(self):
         self.user_similarity = pairwise_distances(self.data_matrix, metric='cosine')
+        print(self.user_similarity)
         return self.user_similarity
     def getResultFor(self,userid):
-        return self.user_similarity[userid]  
+        print(self.user_similarity[userid])
+        return list(self.user_similarity[userid])  
 
